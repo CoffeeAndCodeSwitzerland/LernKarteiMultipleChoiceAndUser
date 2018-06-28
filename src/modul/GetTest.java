0bdbaff9ccba612622728fac296567a209fcdd1c
GiBr03 <GiBr03@NBBESC00603.CoffeeAndCode.ch>
2018-06-28 15:08:38 +0200

    Files almost completet

--- src/modul/GetTest.java
+++ src/modul/GetTest.java
@@ -3,6 +3,7 @@ package modul;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GetTest {
@@ -20,12 +21,15 @@ public class GetTest {
 
 		getFileContent(fileName);
 		getNameAndSignatur();
-		System.out.println(nameOfTest + " " + signaturOfAuthor);
 		getTestChapters();
+		extractQuestions();
 	}
+
 	/**
 	 * Extracts the text of the file as char array
-	 * @param fileContent is the char array
+	 * 
+	 * @param fileContent
+	 *            is the char array
 	 */
 	private static void getFileContent(String name) {
 		try {
@@ -35,58 +39,75 @@ public class GetTest {
 			e.printStackTrace();
 		}
 	}
+
 	/**
-	 * gets the name and signatur.
-	 * Searches first for the name and then secondly for the signature
+	 * gets the name and signatur. Searches first for the name and then secondly for
+	 * the signature
 	 */
 	private static void getNameAndSignatur() {
-		String Name = "";				//Temp string for name
-		String Signature = "";			//Temo string for signatur
-		boolean gotName = false;		//Bool to chekc if the name is already found
-		for (int i = 0; i < fileContent.length; i++) { 
-			if (gotName == false) {		//If the name is not already found search here
+		String Name = ""; // Temp string for name
+		String Signature = ""; // Temo string for signatur
+		boolean gotName = false; // Bool to chekc if the name is already found
+		for (int i = 0; i < fileContent.length; i++) {
+			if (gotName == false) { // If the name is not already found search here
 				if (fileContent[i] == '[') {
 					while (fileContent[i] != ']') {
 						i++;
 						Name += (char) fileContent[i];
 					}
 					gotName = true;
-					Name = Name.replace("]","");//Removes the brackets at the end of the string
+					Name = Name.replace("]", "");// Removes the brackets at the end of the string
 				}
 
-			} else {				
+			} else {
 				if (fileContent[i] == '[') {
-					while(fileContent[i] != ']') {
+					while (fileContent[i] != ']') {
 						i++;
 						Signature += (char) fileContent[i];
 					}
-					Signature = Signature.replace("]","");//Removes the brackets at the end of the string
+					Signature = Signature.replace("]", "");// Removes the brackets at the end of the string
 				}
 			}
 		}
 		nameOfTest = Name;
 		signaturOfAuthor = Signature;
 	}
+
 	/**
 	 * Searches for chapters and splits them
-	 * @return returns an Array list with each chapter on it 
+	 * 
+	 * @return returns an Array list with each chapter on it
 	 */
 	private static ArrayList<String> getTestChapters() {
 		ArrayList<String> chaptersArrayList = new ArrayList();
 		String tempChapter = "";
 		Integer j = -1;
 		for (int i = 0; i < fileContent.length; i++) {
-			if(fileContent[i] == '(') {
-				while(fileContent[i] != ')') {
+			if (fileContent[i] == '(') {
+				while (fileContent[i] != ')') {
 					i++;
 					tempChapter += fileContent[i];
-					tempChapter = tempChapter.replace(")", ""); //Removes the brackets at the end of the string
+					tempChapter = tempChapter.replace(")", ""); // Removes the brackets at the end of the string
 				}
 				j += 1;
-				chaptersArrayList.add(j,tempChapter);	//Adds the chapter to an arraylist
-				tempChapter = "";						//Sets the chapter to be empty again
+				chaptersArrayList.add(j, tempChapter); // Adds the chapter to an arraylist
+				tempChapter = ""; // Sets the chapter to be empty again
 			}
 		}
 		return chaptersArrayList;
+	}
+
+
+	private static ArrayList<String[]> extractQuestions() {
+		ArrayList<String[]> questionsArrayList = new ArrayList();
+		String tempString = null;
+		for (int i = 0; i < getTestChapters().size(); i++) {
+			String[] tempQuestions;
+			tempString = getTestChapters().get(i).toString();
+			tempQuestions = tempString.split(";");
+			questionsArrayList.add(i,tempQuestions);
+			tempString = "";
+		}
+		return questionsArrayList;
 	}
 }
